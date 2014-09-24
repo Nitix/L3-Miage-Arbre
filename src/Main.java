2b3461a9c01c4d76d0f748cd44ce907e53c2ea00
Nitix <guillaume.pierson.gp@gmail.com>
2014-09-24 22:55:38 +0200

    Correction de la suppression en mêm tant que le parcours

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Noeud<Integer> noeud = new Noeud<Integer>(5);
+		noeud.setFeuille(true);
+		noeud.getValeur().add(1);
+		noeud.getValeur().add(2);
+		noeud.getValeur().add(3);
+		noeud.getValeur().add(4);
+		noeud.getValeur().add(5);
+		
+		noeud.split();
+		
+		System.out.println(noeud.toString());
+		System.out.println(noeud.getPointeur().get(0));
+		System.out.println(noeud.getPointeur().get(1));
+
+	}
+
+}
--- src/Noeud.java
+++ src/Noeud.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Noeud<T extends Comparable<T>> {
-	
+
 	// Attributs
 	private int ordre;
 	private double tauxremplissage = 0;
@@ -10,51 +11,68 @@ public class Noeud<T extends Comparable<T>> {
 	private boolean feuille;
 	private boolean racine;
 	private Noeud<T> Pere;
-	
+
 	// Constructeur
 	Noeud(int o){
 		ordre = o;
 		racine = true;
 	}
-	
+
 	Noeud(int o, Noeud<T> p){
 		ordre = o;
 		Pere = p;
 		racine = false;
 	}
-	
+
 	// Méthodes 
 	public void split(){
-		
-		if(this.isRacine() == true){ //Cas ou je split la racine et que racine = feuille. 
+
+		if(this.isRacine() == true){ //Cas ou je split la racine
 			int nbvaleur = this.getOrdre() +1;
-			Noeud<T> fils1 = new Noeud<T>(this.getOrdre(), this);
-			Noeud<T> fils2 = new Noeud<T>(this.getOrdre(), this);
-			
+			Noeud<T> fils1 = new Noeud<T>(this.getOrdre()+1, this);
+			Noeud<T> fils2 = new Noeud<T>(this.getOrdre()+1, this);
+
 			int pair = nbvaleur % 2;
-			for(int i = 0; i<=nbvaleur/2+pair-1; i++){
-				fils1.getValeur().add(this.getValeur().get(i));
-				if(i != nbvaleur){
-					this.getValeur().remove(i);
+			Iterator<T> it = this.valeur.iterator();
+			int actuel = 0;
+			while(it.hasNext()){
+				T valeur = it.next();
+				if(actuel < nbvaleur/2+pair){
+					if(actuel != nbvaleur/2+pair - 1 ){
+						it.remove();
+					}
+					fils1.getValeur().add(valeur);
+				}else{
+					it.remove();
+					fils2.getValeur().add(valeur);
 				}
+				actuel++;
 			}
-			for(int j = nbvaleur/2+pair; j<nbvaleur;j++){
-				fils2.getValeur().add(this.getValeur().get(j));
-				this.getValeur().remove(j);
-			}
-			
+
 			fils1.mettreAJourTauxDeRemplissage();
 			fils2.mettreAJourTauxDeRemplissage();
 			
-			
-			this.getPointeur().set(0, fils1);
-			this.getPointeur().set(1, fils2);
+			if(!this.feuille){
+				for(int i = 0; i < nbvaleur/2+pair; i++){
+					fils1.addNoeud(i, this.pointeur.get(i));
+				}
+				for(int i = nbvaleur/2+pair; i <= nbvaleur; i++){
+					fils2.addNoeud(i - nbvaleur/2+pair,this.pointeur.get(i));
+				}
+				this.pointeur.clear();
+			}
+			this.getPointeur().add(0, fils1);
+			this.getPointeur().add(1, fils2);
 		}else{
 			//TODO
 		}
 	}
-	
-	
+
+
+	public void addNoeud(int index, Noeud<T> noeud) {
+		this.pointeur.set(index, noeud);
+	}
+
 	public void mettreAJourTauxDeRemplissage() {
 		this.tauxremplissage = this.valeur.size() / this.ordre;
 	}
@@ -116,4 +134,16 @@ public class Noeud<T extends Comparable<T>> {
 		Pere = pere;
 	}
 	
+	@Override
+	public String toString() {
+		StringBuilder res = new StringBuilder("| ");
+		Iterator<T> i = this.valeur.iterator();
+		while(i.hasNext()){
+			res.append(i.next());
+			res.append(" | ");
+		}
+		
+		return res.toString();
+	}
+
 }
